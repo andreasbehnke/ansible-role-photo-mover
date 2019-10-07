6ba881850d16402d38ccba26e5d9b2e908ecb3c5
andreasbehnke <ab@andreasbehnke.com>
2019-10-07 11:55:34 +0200

    moved code for moving file into own method and made exif time detection more robust

--- files/move_photos.py
+++ files/move_photos.py
@@ -5,12 +5,11 @@ import time
 
 from exifread import process_file
 
-
 def get_created_date(file_path):
     with open(file_path, 'rb') as img_file:
         exif = process_file(img_file, details=False, stop_tag='DateTimeOriginal')
         created = exif.get('EXIF DateTimeOriginal')
-        if created is None:
+        if created is None or len(str(created)) != 19:  # 2018-12-01 12:34:56
             return time.gmtime(os.path.getmtime(file_path))  # fall back to file modification date
         created_str = str(created)
         # both date formats seems to be present in jpeg files, 2018-12-01 and 2018:12:01
@@ -26,11 +25,24 @@ def get_target_path(suffix, pattern, file_path):
     return suffix + '/' + time.strftime(time_pattern, created)
 
 
-file_path_pattern = '%Y/%m/%Y-%m-%d-%f'
-source = "/mnt/backup"
-target = "/mnt/photo"
+def move_file_if_not_exists(source_path, target_path):
+
+    if os.path.exists(target_path):
+        return False
+    else:
+        print(target_path)
+        target_path_dir = os.path.dirname(target_path)
+        if not os.path.exists(target_path_dir):
+            os.makedirs(target_path_dir)
+        shutil.copy2(source_path, target_path)
+        return True
+
+
 skipped = 0
 copied = 0
+file_path_pattern = '%Y/%m/%Y-%m-%d-%f'
+source = "/home/andreasbehnke/Bilder/Buch"
+target = "/home/andreasbehnke/Bilder/Year"
 
 for dir_path, dir_names, file_names in os.walk(source):
     for file_name in file_names:
@@ -38,14 +50,9 @@ for dir_path, dir_names, file_names in os.walk(source):
                                        'qt')):
             source_path = os.path.join(dir_path, file_name)
             target_path = get_target_path(target, file_path_pattern, source_path)
-            if os.path.exists(target_path):
-                skipped = skipped + 1
-            else:
-                print(target_path)
-                target_path_dir = os.path.dirname(target_path)
-                if not os.path.exists(target_path_dir):
-                    os.makedirs(target_path_dir)
-                shutil.copy2(source_path, target_path)
+            if move_file_if_not_exists(source_path, target_path):
                 copied = copied + 1
+            else:
+                skipped = skipped + 1
 print('skipped existing files: ' + str(skipped))
 print('copied new files: ' + str(copied))
